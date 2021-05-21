7dc5c97aae8a99e7ad880390905f5c5a8358260c
kk488 <78923924+kk488@users.noreply.github.com>
2021-05-20 17:46:53 -0700

    Update task3.py

--- task3.py
+++ task3.py
@@ -15,3 +15,13 @@ example:
 Enter a number: -3
 -3 is not a positive integer
 """
+number=float(input("enter a number:"))
+rem=number%2
+if number<0:
+    print(number," is not a positive integer")
+elif rem==1 or rem==0 and number!=0:
+    print(number," is a positive integer")
+elif number==0:
+    print(" the number is zero")
+else:
+     print(number," is not a positive integer")
