15e5d37647084b11769fb89d2db0d77c50ae1cc1
kk488 <78923924+kk488@users.noreply.github.com>
2021-05-20 17:46:07 -0700

    Update problem2.py

--- problem2.py
+++ problem2.py
@@ -23,3 +23,17 @@ Enter a number: 4
 Enter another number: 25
 4 is not a factor of 25
 """
+number1=int(input("enter first integer:"))
+number2=int(input("enter second integer:"))
+if number1>number2:
+    f1=number1
+    f2=number2
+else:
+    f1=number2
+    f2=number1
+rem=f1%f2
+if rem==0:
+    print(f2," is a factor of ",f1)
+else:
+    print(f2," is not a factor of ",f1)
+    
